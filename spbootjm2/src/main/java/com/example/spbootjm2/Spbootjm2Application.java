e429052765bf7a8352fd63808d19b57412277fbc
CageGao <cagegao@hotmail.com>
2018-04-16 16:40:34 +0800

    SpringBoot+Mybatis mapper.xml

--- spbootjm2/src/main/java/com/example/spbootjm2/Spbootjm2Application.java
+++ spbootjm2/src/main/java/com/example/spbootjm2/Spbootjm2Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.example.spbootjm2.mapper") // 用于完成接口->XML文件的映射
 public class Spbootjm2Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Spbootjm2Application.class, args);
 	}
