48d4f973c07022d893871e846c329201ebb9aee9
CageGao <cagegao@hotmail.com>
2018-04-16 16:45:51 +0800

    SpringBoot Demo

--- spboot1/src/main/java/com/example/spboot1/Spboot1Application.java
+++ spboot1/src/main/java/com/example/spboot1/Spboot1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spboot1Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Spboot1Application.class, args);
 	}
